f5e6f2a97942270621214d84174a5f945e954394
MustaphaBousilDev <bousilmustapha@gmail.com>
2025-02-01 23:56:56 +0100

    creating a dynamic configuration connection with database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
 import { AppConfigModule } from './config/env/config.module';
 import { AppController } from './app.controller';
+import { DatabaseModule } from './config/databases/database.module';
+import { DatabaseProvider } from './config/databases/constants/database.enum';
+import { EnvironmentVariables } from './config/env/env.validation';
 
 @Module({
-  imports: [AppConfigModule],
+  imports: [
+    AppConfigModule,
+    DatabaseModule.forRootAsync({
+      imports: [AppConfigModule],
+      inject: [EnvironmentVariables],
+      useFactory: (env: EnvironmentVariables) => ({
+        provider: DatabaseProvider.PRISMA,
+        config: {
+          type: 'postgres',
+          host: env.dbHost,
+          port: Number(env.dbPort),
+          username: env.dbUsername,
+          password: env.dbPassword,
+          database: env.dbName,
+        },
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [],
 })
--- src/config/databases/database.module.ts
+++ src/config/databases/database.module.ts
@@ -3,6 +3,19 @@ import { DatabaseProvider } from './constants/database.enum';
 import { IDatabaseConfig } from './interfaces/database.interface';
 import { DatabaseFactory } from './factories/database.factory';
 
+interface DatabaseModuleOptions {
+  provider: DatabaseProvider;
+  config: IDatabaseConfig;
+}
+
+interface DatabaseAsyncOptions {
+  imports: any[];
+  useFactory: (
+    ...args: any[]
+  ) => Promise<DatabaseModuleOptions> | DatabaseModuleOptions;
+  inject: any[];
+}
+
 @Module({})
 export class DatabaseModule {
   static forRoot(
@@ -16,6 +29,33 @@ export class DatabaseModule {
         {
           provide: 'DATABASE',
           useValue: database,
+        },
+      ],
+      exports: ['DATABASE'],
+    };
+  }
+
+  static forRootAsync(options: DatabaseAsyncOptions): DynamicModule {
+    return {
+      module: DatabaseModule,
+      imports: options.imports,
+      providers: [
+        {
+          provide: 'DATABASE_OPTIONS',
+          useFactory: options.useFactory,
+          inject: options.inject,
+        },
+        {
+          provide: 'DATABASE',
+          // dbOptions took his value from result of injection inject: ['DATABASE_OPTIONS'],
+          useFactory: (dbOptions: DatabaseModuleOptions) => {
+            //console.log(dbOptions);
+            return DatabaseFactory.createDatabase(
+              dbOptions.provider,
+              dbOptions.config,
+            );
+          },
+          inject: ['DATABASE_OPTIONS'],
         },
       ],
       exports: ['DATABASE'],
